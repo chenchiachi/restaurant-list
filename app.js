6e187f5582d585420e98675cb0d07ce2264be789
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-08-16 15:20:15 +0800

    Add mongoose

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const bodyParser = require('Body-Parser')
+
+const Restaurant = require('./models/restaurant')
 
 const app = express()
 const port = 3000
 
+mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection // 取得資料庫連線狀態
+db.on('error', () => {
+  console.log('mongodb error!') // 連線異常
+})
+db.once('open', () => {
+  console.log('mongodb connected!') // 連線成功
+})
 
 app.get('/', (req, res) => {
   res.send('this is restaurant list project')
