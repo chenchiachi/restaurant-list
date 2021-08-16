9d1f27f7c9f3d9685e037bb839452fa9978b6e0e
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-08-16 18:12:57 +0800

    Add creat todo function

--- app.js
+++ app.js
@@ -20,13 +20,44 @@ db.once('open', () => {
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+
 })
 
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
 
+app.post('/restaurants', (req, res) => {
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const google_map = req.body.goole_map
+  const rating = req.body.rating
+  const description = req.body.description
+  console.log(req.body)
+  return Restaurant.create({
+    name,
+    name_en,
+    category,
+    image,
+    phone,
+    google_map,
+    rating,
+    description
+  })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
 })
