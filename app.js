6f43ece76c5bcee0eae46a5850a13f5b09276b4c
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-08-21 22:58:17 +0800

    Modify express routes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('Body-Parser')
 const methodOverride =require('method-override')
 const Restaurant = require('./models/restaurant')
-
+const routes = require('./routes')
 const app = express()
 const port = 3000
 
@@ -22,104 +22,18 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
-
-app.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
-
-})
-
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-
-app.post('/restaurants', (req, res) => {
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  console.log(req.body)
-  return Restaurant.create({
-    name,
-    name_en,
-    category,
-    image,
-    location,
-    phone,
-    google_map,
-    rating,
-    description
-  })
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
-
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('detail', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('edit', { restaurant }))
-    .catch(error => console.log(error))
-})
-
-app.put('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
-    .then(restaurant => res.redirect(`/restaurants/${id}`))
-    .catch(error => concole.log(error))
-})
-
-app.delete('/restaurants/:id', (req, res) => {
-  const id =req.params.id
-  return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.use(routes)
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
-  Restaurant.find({$or: [{'name': { '$regex': keyword, '$options': 'i' } } ,{'category': { '$regex': keyword, '$options': 'i' } }]})  //參考https://stackoverflow.com/questions/47656515/updateone-on-mongodb-not-working-in-node-js
-  .lean()
-  .then(restaurants => res.render('index', { restaurants ,keyword }))
-  .catch(error => console.log(error))
+  Restaurant.find({ $or: [{ 'name': { '$regex': keyword, '$options': 'i' } }, { 'category': { '$regex': keyword, '$options': 'i' } }] })  //參考https://stackoverflow.com/questions/47656515/updateone-on-mongodb-not-working-in-node-js
+    .lean()
+    .then(restaurants => res.render('index', { restaurants, keyword }))
+    .catch(error => console.log(error))
 })
+
+
+
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
