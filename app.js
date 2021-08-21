3d97ea8389cfa819ed08cac64be7d58805815c25
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-08-21 14:40:42 +0800

    Update search function

--- app.js
+++ app.js
@@ -114,7 +114,7 @@ app.post('/restaurants/:id/delete', (req, res) => {
 
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword.trim()
-  Restaurant.find({ 'name': { '$regex': keyword, '$options': 'i' } })  //參考https://stackoverflow.com/questions/47656515/updateone-on-mongodb-not-working-in-node-js
+  Restaurant.find({$or: [{'name': { '$regex': keyword, '$options': 'i' } } ,{'category': { '$regex': keyword, '$options': 'i' } }]})  //參考https://stackoverflow.com/questions/47656515/updateone-on-mongodb-not-working-in-node-js
   .lean()
   .then(restaurants => res.render('index', { restaurants ,keyword }))
   .catch(error => console.log(error))
