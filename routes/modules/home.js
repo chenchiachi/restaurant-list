a5057ea134dad91da534ae619bb254d9c5ec3466
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2021-08-22 21:46:07 +0800

    Modify sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,12 +2,37 @@ const express = require('express')
 const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
-
 router.get('/', (req, res) => {
-  Restaurant.find()
-    .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
+  const sorting = req.query.sorting
+  let sortRule = {}
+  const keyword = (req.query.keyword || '')
+  let searchRule = {}
+  if (keyword) {
+    searchRule = { $or: [{ 'name': { '$regex': keyword, '$options': 'i' } }, { 'category': { '$regex': keyword, '$options': 'i' } }]}
+  }
+
+  switch (sorting) {
+  case 'AtoZ':
+    sortRule = { name: 'asc' }
+    break
+  case 'ZtoA':
+    sortRule = { name: 'desc' }
+    break
+  case 'category':
+    sortRule = { category: 'asc' }
+    break
+  case 'location':
+    sortRule = { location: 'asc' }
+    break
+  default:
+    sortRule = { _id: 'asc' }
+}
+
+Restaurant.find(searchRule)
+  .lean()
+  .sort(sortRule)
+  .then(restaurants => res.render('index', { restaurants,  keyword }))
+  .catch(error => console.log(error))
 })
 
 module.exports = router
