3ba2618060f42b672fa1e422a08996bac1669561
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2022-01-02 15:11:47 +0100

    Add message partials

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -7,10 +7,24 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/login', passport.authenticate('local', {
-  successRedirect: '/',
-  failureRedirect: '/users/login'
-}))
+router.post('/login', function (req, res, next) {
+  const { email, password } = req.body
+  if (!email || !password) {
+    req.flash('warning_msg', '請輸入Email和密碼。')
+    return res.redirect('/users/login')
+  }
+  passport.authenticate('local', function (err, user, info) {
+    if (err) { return next(err); }
+    if (!user) {
+      req.flash('warning_msg', info.message)
+      return res.redirect('/users/login')
+    }
+    req.logIn(user, function (err) {
+      if (err) { return next(err) }
+      return res.redirect('/')
+    })
+  })(req, res, next)
+})
 
 router.get('/register', (req, res) => {
   res.render('register')
@@ -18,29 +32,49 @@ router.get('/register', (req, res) => {
 
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
+  const errors = []
+  if (!email) {
+    errors.push({ message: '請填寫email。' })
+  }
+  if (!password) {
+    errors.push({ message: '請填寫密碼。' })
+  }
+  if (password !== confirmPassword) {
+    errors.push({ message: '密碼與確認密碼不符。' })
+  }
+  if (errors.length) {
+    return res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      confirmPassword
+    })
+  }
   User.findOne({ email }).then(user => {
     if (user) {
-      console.log('User already exists.')
-      res.render('register', {
+      errors.push({ message: '此Email已註冊。'})
+      return res.render('register', {
+        errors,
         name,
         email,
         password,
         confirmPassword
       })
-    } else {
-      return User.create({
+    } 
+    return User.create({
         name,
         email,
         password
       })
         .then(() => res.redirect('/'))
         .catch(err => console.log(err))
-    }
   })
 })
 
 router.get('/logout', (req, res) => {
   req.logout()
+  req.flash('success_msg', '已登出。')
   res.redirect('/users/login')
 })
 
